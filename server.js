1281ef7b02152605432d4a86f0332baed25d5301
ryanj <ryan.jarvinen@gmail.com>
2014-11-07 16:39:27 -0800

    adding redirect support

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ var cc          = require('config-multipaas'),
     restify     = require('restify'),
     fs          = require('fs')
 
-var config      = cc().add({DEFAULT_BTN_TEXT: process.env.DEFAULT_BTN_TEXT || "LAUNCH ON"}),
+var launchthis  = require('./package.json').openshift.launch_url
+var config      = cc().add({
+      DEFAULT_BTN_TEXT: process.env.DEFAULT_BTN_TEXT || "LAUNCH ON",
+      DEFAULT_LAUNCH_URL: process.env.LAUNCH_URL || launchthis }),
     app         = restify.createServer()
 
 app.use(restify.queryParser())
 app.use(restify.CORS())
 app.use(restify.fullResponse())
+
+var cleanbuttontext = function (text)
+{
+  text = text || ''
+  if ( text.search(".svg") >= 0 ){
+    text = text.slice(0, text.search(".svg"))
+  }
+  if (text == ''){ 
+    text = config.get('DEFAULT_BTN_TEXT')
+  }
+  return text
+}
+
+var svgtemplate = function (req, res, next)
+{
+  var buttontext = cleanbuttontext(req.params.text)
+  var data = fs.readFileSync(__dirname + '/static/img/launchbutton.svg');
+  console.log('button: {text: "'+buttontext+'"}')
+  res.status(200);
+  res.header('Content-Type', 'image/svg+xml');
+  res.end(data.toString().replace(/LAUNCH ON/, buttontext));
+};
 
 // Routes
 app.get('/status', function (req, res, next)
@@ -15,26 +40,30 @@ app.get('/status', function (req, res, next)
   res.send("{status: 'ok'}");
 });
 
-var getbutton = function (req, res, next)
+//Old urls, deprecated:
+app.get('/button/:text', svgtemplate)
+app.get('/button.svg', svgtemplate)
+
+//SVG template urls:
+app.get('/launch/light.svg', svgtemplate)
+app.get('/launch/light/:text', svgtemplate)
+app.get('/launch.svg', svgtemplate)
+app.get('/launch/:text', svgtemplate)
+app.get('/counter/:id', svgtemplate)
+
+//Counter
+app.get('/r/:id', function (req, res, next)
 {
-  var text = req.params.text || ''
-  if ( text.search(".svg") >= 0 ){
-    text = text.slice(0, text.search(".svg"))
-  }
-  if (text == ''){ 
-    text = config.get('DEFAULT_BTN_TEXT')
-  }
-  
-  var data = fs.readFileSync(__dirname + '/static/img/launchbutton.svg');
-  console.log('button: {text: "'+text+'"}')
-  res.status(200);
-  res.header('Content-Type', 'image/svg+xml');
-  res.end(data.toString().replace(/LAUNCH ON/, text));
-};
+  var id = req.params.id || '123'
+  var url = req.query.url || config.get('DEFAULT_LAUNCH_URL')
+  //emit event w/ timestamp
+  //write to log / db in event handler
+  console.log("launch: { id: \""+id+"\", url: \""+url+"\" }");
+  res.header('Location', url);
+  res.send(302);
+})
 
-app.get('/button.svg', getbutton)
-app.get('/button/:text', getbutton)
-
+// Input form:
 app.get('/', function (req, res, next)
 {
   var data = fs.readFileSync(__dirname + '/index.html');
