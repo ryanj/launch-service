ed4ba490dd673286f78cef733246de82a51bfe54
ryanj <ryan.jarvinen@gmail.com>
2014-11-21 14:11:24 -0800

    cleaner UI

--- bin/client.js
+++ bin/client.js
@@ -3,17 +3,6 @@ require('angular');
 angular.module('launchApp', [])
 .controller('launchController', ['$scope', function($scope) {
   $scope.launcher = {}; 
-  $scope.qs_allowed = ['text','name','initial_git_url','launch_host','launch_path','path','cartridges']
-  $scope.getDefaults = function(){
-    window.location.search.slice(1).split("&").forEach(function(param){
-      var p = param.split("=");
-      if( $scope.qs_allowed.indexOf(p[0]) >= 0){
-        $scope.defaults[p[0]] = p[1];
-      }
-    })
-    $scope.defaults.url=$scope.getLaunchUrl();
-    $scope.launcher.url=$scope.defaults.url;
-  };
   $scope.defaults = {
     'host' : window.location.protocol + "//" + window.location.host + '/',
     'text' : "LAUNCH ON",
@@ -23,7 +12,20 @@ angular.module('launchApp', [])
     'launch_path'    : "/app/console/application_type/custom",
     'cartridges'     : "nodejs-0.10",
     'initial_git_url': "https%3A%2F%2Fgithub.com%2Fryanj%2Flaunch-service.git"
-  }
+  };
+  $scope.getQSDefaults = function(){
+    var qs_whitelist = ['text','name','initial_git_url','launch_host','launch_path','path','cartridges']
+    window.location.search.slice(1).split("&").forEach(function(param){
+      var p = param.split("=");
+      if( qs_whitelist.indexOf(p[0]) >= 0){
+        $scope.defaults[p[0]] = p[1];
+      }
+    })
+    $scope.defaults.url=$scope.getLaunchUrl();
+    $scope.launcher.url=$scope.defaults.url;
+    $scope.onUrlChange()
+    $scope.onTextChange()
+  };
   $scope.createLaunchUrl = function(launch_host, launch_path, cartridges, initial_git_url, name){
     carts = '?';
     cart_params = cartridges.split(',');
@@ -50,19 +52,17 @@ angular.module('launchApp', [])
   $scope.getImageUrl = function(text){
     return $scope.defaults.host + 'button/'+text+'.svg'
   }
-  $scope.urlchange = function() {
+  $scope.onUrlChange = function() {
     $scope.launcher.url = $scope.launcher.url || $scope.defaults.url;
     $scope.launcher.html = $scope.getHtml($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
     $scope.launcher.markdown = $scope.getMarkdown($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
   };
-  $scope.textchange = function() {
+  $scope.onTextChange = function() {
     $scope.launcher.text = $scope.launcher.text || $scope.defaults.text;
     $scope.launcher.alt = $scope.launcher.text + " OpenShift";
     $scope.launcher.image = $scope.getImageUrl($scope.launcher.text)
     $scope.launcher.html = $scope.getHtml($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
     $scope.launcher.markdown = $scope.getMarkdown($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
   };
-  $scope.getDefaults()
-  $scope.textchange()
-  $scope.urlchange()
+  $scope.getQSDefaults()
 }]);
