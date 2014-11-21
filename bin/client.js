69021ab10f441fcde254ab08e7e46b39f2e09fd4
ryanj <ryan.jarvinen@gmail.com>
2014-11-20 19:27:02 -0800

    adding support for launching to any openshift host

--- bin/client.js
+++ bin/client.js
@@ -3,7 +3,7 @@ require('angular');
 angular.module('launchApp', [])
 .controller('launchController', ['$scope', function($scope) {
   $scope.launcher = {}; 
-  $scope.qs_allowed = ['url', 'text']
+  $scope.qs_allowed = ['text','name','initial_git_url','launch_host','launch_path','path','cartridges']
   $scope.getDefaults = function(){
     window.location.search.slice(1).split("&").forEach(function(param){
       var p = param.split("=");
@@ -11,12 +11,35 @@ angular.module('launchApp', [])
         $scope.defaults[p[0]] = p[1];
       }
     })
+    $scope.defaults.url=$scope.getLaunchUrl();
+    $scope.launcher.url=$scope.defaults.url;
   };
   $scope.defaults = {
     'host' : window.location.protocol + "//" + window.location.host + '/',
     'text' : "LAUNCH ON",
     'image': "button.svg",
-    'url'  : 'https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=nodejs-0.10&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Flaunch-service.git&name=launch'
+    'name' : "launch",
+    'launch_host'    : "https://openshift.redhat.com",
+    'launch_path'    : "/app/console/application_type/custom",
+    'cartridges'     : "nodejs-0.10",
+    'initial_git_url': "https%3A%2F%2Fgithub.com%2Fryanj%2Flaunch-service.git"
+  }
+  $scope.createLaunchUrl = function(launch_host, launch_path, cartridges, initial_git_url, name){
+    carts = '?';
+    cart_params = cartridges.split(',');
+    cart_params.forEach(function(cart){
+      carts += 'cartridges[]='+cart+'&';
+    });
+    return launch_host+launch_path+carts+"initial_git_url="+initial_git_url+"&name="+name 
+  }
+  $scope.getLaunchUrl = function(){
+    return $scope.createLaunchUrl(
+      $scope.defaults.launch_host,
+      $scope.defaults.launch_path,
+      $scope.defaults.cartridges,
+      $scope.defaults.initial_git_url,
+      $scope.defaults.name
+    );
   }
   $scope.getMarkdown = function(url, alt, image){
     return "[![" + image + "](" + alt + ")](" + url + ")";
