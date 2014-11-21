05785baef6fd7f77cc71683f8deacd063be9bda3
ryanj <ryan.jarvinen@gmail.com>
2014-11-20 17:34:10 -0800

    basic support for defaulting form inputs via querystring (#3)

--- bin/client.js
+++ bin/client.js
@@ -3,11 +3,20 @@ require('angular');
 angular.module('launchApp', [])
 .controller('launchController', ['$scope', function($scope) {
   $scope.launcher = {}; 
+  $scope.qs_allowed = ['url', 'text']
+  $scope.getDefaults = function(){
+    window.location.search.slice(1).split("&").forEach(function(param){
+      var p = param.split("=");
+      if( $scope.qs_allowed.indexOf(p[0]) >= 0){
+        $scope.defaults[p[0]] = p[1];
+      }
+    })
+  };
   $scope.defaults = {
-    'image_host': window.location.protocol + "//" + window.location.host + '/', 
-    'text' : process.env.DEFAULT_BTN_TEXT   || "LAUNCH ON",
-    'image': process.env.DEFAULT_IMG_URL    || "button.svg",
-    'url'  : process.env.DEFAULT_LAUNCH_URL || 'https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=nodejs-0.10&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Flaunch-service.git&name=launch'
+    'host' : window.location.protocol + "//" + window.location.host + '/',
+    'text' : "LAUNCH ON",
+    'image': "button.svg",
+    'url'  : 'https://openshift.redhat.com/app/console/application_type/custom?cartridges%5B%5D=nodejs-0.10&initial_git_url=https%3A%2F%2Fgithub.com%2Fryanj%2Flaunch-service.git&name=launch'
   }
   $scope.getMarkdown = function(url, alt, image){
     return "[![" + image + "](" + alt + ")](" + url + ")";
@@ -16,7 +25,7 @@ angular.module('launchApp', [])
     return "<a href=\"" + url + "\"><img alt=\"" + alt + "\" src=\"" + image + "\" /></a>";
   };
   $scope.getImageUrl = function(text){
-    return $scope.defaults.image_host + 'button/'+text+'.svg'
+    return $scope.defaults.host + 'button/'+text+'.svg'
   }
   $scope.urlchange = function() {
     $scope.launcher.url = $scope.launcher.url || $scope.defaults.url;
@@ -30,6 +39,7 @@ angular.module('launchApp', [])
     $scope.launcher.html = $scope.getHtml($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
     $scope.launcher.markdown = $scope.getMarkdown($scope.launcher.url, $scope.launcher.alt, $scope.launcher.image);
   };
+  $scope.getDefaults()
   $scope.textchange()
   $scope.urlchange()
 }]);
